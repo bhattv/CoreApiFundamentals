66bcc64a300c5288457ec21eb88cde3ee1757234
bhattv <bhattv@gmail.com>
2020-02-25 19:30:19 -0500

    Update 1

---
+++ src/Data/CampProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+
+namespace CoreCodeCamp
+{
+    public class CampProfile : Profile
+    {
+        public CampProfile()
+        {
+            this.CreateMap<Camp, CampModel>();
+
+        }
+    }
+}
